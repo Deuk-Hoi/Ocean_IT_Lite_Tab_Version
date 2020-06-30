c74ea4537aaf2094eb18c05eccb5f1422faf4c11
Deuk_Hoi <dh97k@naver.com>
2020-06-30 16:54:20 +0900

    Initial Commit

---
+++ Ocean IT Lite2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Ocean IT Lite2
+//
+//  Created by hoseoict on 2020/06/30.
+//  Copyright © 2020 Ocean IT. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Ocean IT Lite2/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Ocean IT Lite2
+//
+//  Created by hoseoict on 2020/06/30.
+//  Copyright © 2020 Ocean IT. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
